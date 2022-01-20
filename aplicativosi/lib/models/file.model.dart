e1d92878bc395ed6d7b73e1bae1717a154385774
Alexsander Horn <48638282+alexsanderph@users.noreply.github.com>
2022-01-20 13:10:19 -0300

    Update file.model.dart

--- aplicativosi/lib/models/file.model.dart
+++ aplicativosi/lib/models/file.model.dart
@@ -1,7 +1,4 @@
 import 'dart:async';
-import 'dart:io';
-import 'package:flutter/foundation.dart';
-import 'package:open_file/open_file.dart';
 
 typedef Json = Map<String, dynamic>;
 
@@ -34,33 +31,4 @@ class SIFile {
         'description': description,
         'link': link,
       };
-
-  void checkFile() {
-    downloaded = File(path + name).existsSync();
-  }
-
-  void openFile() {
-    OpenFile.open(path + name);
-  }
-
-  Future<bool> downloadFile() async {
-    HttpClient httpClient = new HttpClient();
-    File file;
-    bool downloaded = false;
-    try {
-      var request = await httpClient.getUrl(Uri.parse(link));
-      var response = await request.close();
-      if (response.statusCode == 200) {
-        var bytes = await consolidateHttpClientResponseBytes(response);
-        file = File(path + name);
-        await file.writeAsBytes(bytes);
-        downloaded = true;
-      } else
-        downloaded = false;
-    } catch (ex) {
-      downloaded = false;
-    }
-
-    return downloaded;
-  }
 }
